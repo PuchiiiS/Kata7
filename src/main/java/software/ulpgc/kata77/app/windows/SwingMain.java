23be79c094eac7d4ab006eecd1bfb97c033fb2b1
Estados <hsuliman@gmail.com>
2024-12-30 17:02:12 +0000

    FINISH Kata7

--- src/main/java/software/ulpgc/kata77/app/windows/SwingMain.java
+++ src/main/java/software/ulpgc/kata77/app/windows/SwingMain.java
@@ -1,4 +1,18 @@
 package software.ulpgc.kata77.app.windows;
 
+import javax.swing.*;
+
 public class SwingMain {
+
+    public static void main(String[] args) {
+        new JFrame() {
+
+            {
+                this.setTitle("Kata7");
+                this.setSize(400,400);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            }
+        }.setVisible(true);
+    }
 }
